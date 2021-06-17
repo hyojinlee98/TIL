6d7b955a4e9e500850f1aacb56f754826b28d62c
hyojinlee98 <dnfmfmzhkd12@naver.com>
2021-06-18 01:12:40 +0900

    밑바닥부터 시작하는 딥러닝 실습 코드 수정 및 chapter 4 추가

--- DeepLearning/Chapter4/cee.py
+++
@@ -1,6 +0,0 @@
-# 4.2.2
-import numpy as np
-
-def cross_entropy_error(y, t) :
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
--- DeepLearning/Chapter4/mse.py
+++
@@ -1,5 +0,0 @@
-# 4.2.1_
-import numpy as np
-
-def mean_squared_error(y, t) :
-    return 0.5 * np.sum((y-t)**2)
